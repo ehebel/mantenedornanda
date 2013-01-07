2b74f88a3d22b518066c3f6fd2b75103584bad58
Esteban Hebel <ehebel@gmail.com>
2013-01-07 11:30:48 -0300

    creacion modeladorcie9

---
+++ modeladorcie9/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from mantenedornanda.modeladorcie9.models import *
+
+admin.site.register(cas_concepto)
+admin.site.register(cas_descripcion)
+admin.site.register(cas_conj_mapeo)
+admin.site.register(cas_mapsalida)
+admin.site.register(cas_mapdestino)
+admin.site.register(cas_mapeo)
+
+
+admin.site.register(cienueve)
+admin.site.register(cas_procedimiento)
+__author__ = 'ehebel'
