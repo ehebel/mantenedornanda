0c30be921a0228d253d8de61a115d2ab0411be30
Esteban Hebel <ehebel@gmail.com>
2013-03-13 12:57:33 -0300

    mejorado admin Kairos clave compuesta

--- modeladorcie9/models.py
+++ modeladorcie9/models.py
@@ -71,7 +71,7 @@ class cienueve(models.Model):
     area = models.CharField(max_length=5)
     clasificacion = models.CharField(max_length=5)
     def __unicode__(self):
-        return self.descriptor
+        return self.codigo
     class Meta:
         ordering=['codigo']
 
